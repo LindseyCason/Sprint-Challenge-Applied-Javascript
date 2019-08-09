46025a3a89d47cce9070ff91e7b2fc043ff36d89
Lindsey Cason <lindseyacason@gmail.com>
2019-08-09 12:14:19 -0500

    complete without stretch

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,66 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+ 
+let cardContainer = document.querySelector('.cards-container');
+let authorNames = [];
+let imageURLs = [];
+let headlines = [];
+ 
+ 
+ 
+ 
+ 
+ //get axios data
+ 
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+.then(response=>{ 
+ let art = response.data.articles
+ console.log(response); //Obj
+ console.log(art); //Obj containing 4 topic arrays, each array containing all 3 items needed //LOOP THIS
+ globalTopic.forEach(topic =>{ //globalTopic is an array created on the header page//using here to save space
+       let topicArr = art[topic]; //might not need this variable but makes it easier to read for me
+       for(let i=0; i<topicArr.length; i++){
+       let auth = topicArr[i]["authorName"]
+       let pic = topicArr[i]["authorPhoto"];
+       let head = topicArr[i]["headline"]
+       cardContainer.appendChild(createCard(auth, pic, head))
+ 
+       }
+})
+})
+.catch(error =>{
+ console.log(error);
+})
+ 
+ 
+function createCard(auth, pic, head){
+   const card = document.createElement('div')       
+   const headline = document.createElement('div')
+   const author = document.createElement('div');
+   const imageContainer = document.createElement('div');
+   const image = document.createElement('img');
+   const authorSpan = document.createElement('span');
+ 
+   card.appendChild(headline);
+   card.appendChild(author);
+   author.appendChild(imageContainer);
+   imageContainer.appendChild(image)
+   image.src = pic;
+   card.appendChild(authorSpan);
+ 
+   card.classList.add('card');
+   headline.classList.add('headline');       
+   author.classList.add('author');
+   imageContainer.classList.add('img-container');
+ 
+   headline.textContent = head;
+   authorSpan.textContent = "By: " + auth;
+ 
+return card;
+ 
+}
+ 
+ 
+ 
+// console.log(globalTopic);
